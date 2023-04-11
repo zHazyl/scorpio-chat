1e0ed456057d69cae70080baea2b9ab912daa62d
Admin <ghan301002@gmail.com>
2023-04-11 23:45:45 +0700

    Han|auth-service

---
+++ scorpio-be/auth-service/src/main/java/com/tnh/authservice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.tnh.authservice.repository;
+
+import com.tnh.authservice.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
